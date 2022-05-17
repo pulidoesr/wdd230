b6c863f948ae18bc106812aa81559a65953262c4
pulidoesr <pul21010@byui.edu>
2022-05-16 23:34:34 -0500

    w4 late corrections

--- chamber/js/scripts.js
+++ chamber/js/scripts.js
@@ -5,5 +5,24 @@ function toggleMenu() {
 }
 const x = document.getElementById('hamburgerBtn');
   x.onclick = toggleMenu;
+// select the elements to manipulate (output to)
+const datefield = document.querySelector(".date"); 
+const datefieldUK = document.querySelector("aside"); // for european/family history format with day first.
+
+// derive the current date using a date object
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+	now);
+const fulldateUK = new Intl.DateTimeFormat("en-UK", {
+	dateStyle: "full"
+}).format(now);
+// long, medium, short options ... try them
+
+/* datefield.innerHTML = `<em>${fulldate}</em>`; */
+datefieldUK.innerHTML = `<em>${fulldateUK}</em>`;
+
+
+
+
 let oLastModif = new Date(document.lastModified);
 document.getElementById('lastmodified').textContent = oLastModif;
