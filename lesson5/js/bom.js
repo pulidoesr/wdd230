1f8d151218dec23206ce6e5618a06915c60218fd
pulidoesr <pul21010@byui.edu>
2022-05-19 23:11:35 -0500

    W5 BOM Icon

--- lesson5/js/bom.js
+++ lesson5/js/bom.js
@@ -13,7 +13,7 @@ button.addEventListener('click', () => {
         listItem.appendChild(listText);
         listText.textContent = myItem;
         listItem.appendChild(listBtn);
-        listBtn.textContent = 'Delete';
+        listBtn.textContent = '❌';
         list.appendChild(listItem);
 
         listBtn.addEventListener('click', () => {
