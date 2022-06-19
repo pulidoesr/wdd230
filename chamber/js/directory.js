fb2654d0f5851075a5c6b6128c7863d34ec803a1
pulidoesr <pul21010@byui.edu>
2022-06-19 00:47:39 -0500

    w09 json updates

--- chamber/js/directory.js
+++ chamber/js/directory.js
@@ -15,28 +15,31 @@ function displayCompanies(company) {
   // Create elements to add to the documewnt
   let card = document.createElement('section');
   let h2 = document.createElement('h2');
+  let logo = document.createElement('img');
   let address = document.createElement('p');
   let phone = document.createElement('p');
   let webpage = document.createElement('p');
-  let logo = document.createElement('img');
+  
 
 // Change the textContent property of the h2 element to contain the prophet's full name
 h2.textContent = `${company.name}`;
 
 // Change the textContent property of the additional element to contain the data
-address.textContent= `Address: ${company.address}`
-phone.textContent= `Phone: ${company.phone}`
-webpage.textContent= `Web Page: ${company.webpage}`
+address.textContent= `${company.address}`
+phone.textContent= `${company.phone}`
+webpage.textContent= `https://${company.webpage}`
 
 // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
-logo.setAttribute('src', `image/${company.logo}`);
+logo.setAttribute('src', `images/${company.logo}`);
+logo.setAttribute('style', `width:50px;height:55px`);
 
  // Add/append the section(card) with the h2 element
  card.appendChild(h2);
+ card.appendChild(logo);
  card.appendChild(address);
  card.appendChild(phone);
  card.appendChild(webpage);
- card.appendChild(logo);
+ 
   // Add/append the existing HTML div with the cards class with the section(card)
   document.querySelector('div.cards').appendChild(card);
 }
