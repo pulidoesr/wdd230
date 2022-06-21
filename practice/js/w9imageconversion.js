33d4e298b77f1e69d1e18d760d2bb09859c636ca
pulidoesr <pul21010@byui.edu>
2022-06-20 20:39:25 -0500

    W10 CSS updates

--- practice/js/w9imageconversion.js
+++ practice/js/w9imageconversion.js
@@ -20,12 +20,14 @@ function dropHandler(ev) {
       if (ev.dataTransfer.items[i].kind === 'file') {
         const file = ev.dataTransfer.items[i].getAsFile();
         console.log('... file[' + i + '].name = ' + file.name);
+        displayFile(file);
       }
     }
   } else {
     // Use DataTransfer interface to access the file(s)
     for (let i = 0; i < ev.dataTransfer.files.length; i++) {
       console.log('... file[' + i + '].name = ' + ev.dataTransfer.files[i].name);
+      displayFile(file);
     }
   }
 }
@@ -34,4 +36,16 @@ function dragOverHandler(ev) {
 
   // Prevent default behavior (Prevent file from being opened)
   ev.preventDefault();
+}
+function displayFile(file_image) {
+  // Create elements to add to the documewnt
+  let drop_zone = document.
+  let image_file = document.createElement('p');
+
+
+ // Add/append the section(card) with the h2 element
+ image_file.appendChild(file_image);
+ 
+  // Add/append the existing HTML div with the cards class with the section(card)
+  document.querySelector('div.drop_file').appendChild(image_file);
 }
