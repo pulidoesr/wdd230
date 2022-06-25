dcff9ef3ee193430e21b20f903b32bfb67c75217
pulidoesr <pul21010@byui.edu>
2022-06-25 14:38:30 -0500

    w10 weather api

--- chamber/js/windchill.js
+++ chamber/js/windchill.js
@@ -1,26 +1,32 @@
 const parent = document.getElementById("wind_chill"); // selecting parent
-const temperature = document.getElementById("t"); // temperature
-const speed = document.getElementById("s"); // wind speed
-const oldElement = document.getElementById("wc"); // windchill old
-const newElement = document.createElement("wc"); // windchill new
-let f = 0 // wind chill factor
-let t = parseFloat(temperature.innerText)
-let s = parseFloat(speed.innerText)
-let string = ""
+const temperature = document.getElementById("current_temp"); // temperature
+const speed = document.getElementById("wind_speed"); // wind speed
+/* const oldElement = document.getElementById("wc"); // windchill old
+const newElement = document.createElement("wc"); // windchill new */
+
+if (temperature != null) {
+    
+    let t = parseFloat(temperature.innerText)
+    let s = parseFloat(speed.innerText)
+    let string = ""
 // speed from km. to miles
-s = s * 0.621371; 
+    s = s * 0.621371; 
 // calculation of wind chill
-if (t <= 50 && s > 3) {
-      f = 35.74 + 0.6215*t  -35.75*s**0.16  + 0.4275*t*s**0.16;
-      f = f.toFixed(2);
-      string = f.toString();
-      newElement.innerHTML = string;
-} else {
-      newElement.innerHTML = "N/A";
-}
+    if (t <= 50 && s > 3) {
+          f = 35.74 + 0.6215*t  -35.75*s**0.16  + 0.4275*t*s**0.16;
+        f = f.toFixed(2);
+        string = f.toString();
+        parent.innerHTML = string;
+    } else {
+          parent.innerHTML = "N/A";
+            }
+    } else {
+        parent.innerHTML = "N/A";
+    };
+
 // create or replace the wind chill value
-if (oldElement.hasChildNodes()) {
+/* if (oldElement.hasChildNodes()) {
     parent.replaceChild(newElement, oldElement);
 } else {
     parent.append(newElement);
-}
+} */
