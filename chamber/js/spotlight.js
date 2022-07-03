081f3ef92d667215e11a5034f370a2d990427719
pulidoesr <pul21010@byui.edu>
2022-07-02 19:42:25 -0500

    w11 spotlight members update

--- chamber/js/spotlight.js
+++ chamber/js/spotlight.js
@@ -1,5 +1,6 @@
 const requestURL = 'https://pulidoesr.github.io/wdd230/chamber/data/data.json';
-const container = document.getElementById("directory_list");
+const container = document.getElementById("member_list");
+let count = 0;
 // Read the objects
 fetch(requestURL)
   .then(function (response) {
@@ -8,59 +9,46 @@ fetch(requestURL)
   .then(function (jsonObject) { 
     console.table(jsonObject);  // temporary checking for valid response and data parsing
     const companies = jsonObject['companies'];
-    companies.forEach(displayCompanies);
+    companies.forEach(displayCompanies, count);
   });
 
-function displayCompanies(company) {
+function displayCompanies(company, count) {
   // Create elements to add to the documewnt
   let card = document.createElement('section');
-  let h2 = document.createElement('h2');
+  let h4 = document.createElement('h4');
   let logo = document.createElement('img');
   let address = document.createElement('p');
   let phone = document.createElement('p');
   let webpage = document.createElement('a');
+  let member = document.createElement('p')
+
+count = count + 1
   
 // Change the textContent property of the h2 element to contain name of company
-h2.textContent = `${company.name}`;
+h4.textContent = `${company.name}`;
 
 // Change the textContent property of the additional element to contain the data
 address.textContent= `${company.address}`;
 phone.textContent= `${company.phone}`;
 webpage.textContent= `https://${company.webpage}`;
+member.textContent = `${company.member_level}`
 
 // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. 
 logo.setAttribute('src', `images/${company.logo}`);
-logo.setAttribute('style', `width:100px;height:105px`);
+logo.setAttribute('style', `width:50px;height:55px`);
 webpage.setAttribute('href', `https://${company.webpage}`);
-card.setAttribute('class',"individual");
-
+card.setAttribute('class','member_item');
 
  // Add/append the section(card) with the h2 element
- card.appendChild(h2);
+ card.appendChild(h4);
  card.appendChild(logo);
  card.appendChild(address);
  card.appendChild(phone);
  card.appendChild(webpage);
+ card.appendChild(member);
  
   // Add/append the existing HTML div with the cards class with the section(card)
-  document.querySelector('#directory_list').appendChild(card);
+  if (count <= 3) {
+     document.querySelector('#member_list').appendChild(card);
+  }
 }
-
-const gridbutton = document.querySelector("#grid");
-const listbutton = document.querySelector("#list");
-const display = document.querySelector("#directory_list");
-
-// Grid Button
-gridbutton.addEventListener("click", () => {
-	display.classList.add("grid");
-	display.classList.remove("list");
-  
-});
-
-// List Button
-listbutton.addEventListener("click", showList); 
-function showList() {
-	display.classList.add("list");
-	display.classList.remove("grid");
-}
-
