cd752f911405ccc8459a426195147cc54eb00b5d
pulidoesr <pul21010@byui.edu>
2022-07-19 22:30:39 -0500

    w14 json

--- lesson12/js/main.js
+++ lesson12/js/main.js
@@ -10,17 +10,44 @@ const x = document.getElementById('hamburgerBtn');
 let oLastModif = new Date(document.lastModified);
 document.getElementById('lastmodified').textContent = oLastModif;
 
-
+// Read the cities where we have Temples
 const requestURL = 'https://pulidoesr.github.io/wdd230/lesson12/data/hotel_city.json';
 const container = document.getElementById("#temple_city_select");
+var cities;
 
-// Read the objects
 fetch(requestURL)
   .then(function (response) {
     return response.json();
   })
   .then(function (jsonObject) { 
-    console.table(jsonObject);  // temporary checking for valid response and data parsing
-    const cities = jsonObject['cities'];
-    console.log(cities);
-});
+    cities = jsonObject['cities'];
+    let menu = document.getElementById('city_select');
+//    let option = document.createElement('option');
+    for (var field in cities) {
+        let option = document.createElement('option');
+        option.textContent=`${cities[field].City}`;
+        option.setAttribute('value', `${cities[field].City}`);
+        menu.add(option);
+      }
+  });
+
+
+// Selected City triggers localStorage and Map Update
+function location_change() {
+  var select = document.getElementById("city_select");
+  var source = document.getElementById("googlemap");
+  var city = document.getElementById("scity");
+  var state = document.getElementById("sstate");
+  var country = document.getElementById("scountry");
+  var string = "https://www.google.com/maps/embed/v1/place?key=AIzaSyAcdvUWlfP3YF1AfxIttw1XuveCT9UgNxE&q="
+  city = select.options[select.selectedIndex].value;
+  state = cities[select.selectedIndex].State;
+  country = cities[select.selectedIndex].Country;
+  // change the google map
+  source.src = `${string}${city},${state},${country}`;
+
+  // store the new number of visits value
+  localStorage.setItem("city",city);
+  localStorage.setItem("state",state);
+  localStorage.setItem("country",country); 
+}
